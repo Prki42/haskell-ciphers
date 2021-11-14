fb7864a182effe1692f2fc40fbea996663941a35
Aleksa Prtenjaca <aleksa.prtenjaca03@gmail.com>
2021-11-14 16:09:35 +0100

    Formatting fix

--- Ciphers/Caesar.hs
+++ Ciphers/Caesar.hs
@@ -1,16 +1,16 @@
 module Ciphers.Caesar where
 
-import Ciphers.AlphaHelper (alphaToInt, intToAlpha, isAsciiAlpha)
-import Data.Maybe (catMaybes, mapMaybe)
+import           Ciphers.AlphaHelper (alphaToInt, intToAlpha, isAsciiAlpha)
+import           Data.Maybe          (catMaybes, mapMaybe)
 
 caesarEncrypt :: Int -> String -> String
 caesarEncrypt shift msg =
-  unwords $
-    map
-      ( mapMaybe (intToAlpha . (flip mod 26 . (+ shift)))
-          . mapMaybe alphaToInt
-      )
-      $ words msg
+    unwords
+    $ map (
+        mapMaybe (intToAlpha . (flip mod 26 . (+shift)))
+        . mapMaybe alphaToInt
+    )
+    $ words msg
 
 caesarDecrypt :: Int -> String -> String
 caesarDecrypt shift = caesarEncrypt (negate shift)
--- Main.hs
+++ Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           Ciphers.Affine   (affineDecrypt, affineEncrypt)
 import           Ciphers.Atbash   (atbashTransform)
-import           Ciphers.Autokey  (autokeyEncrypt)
+import           Ciphers.Autokey  (autokeyDecrypt, autokeyEncrypt)
 import           Ciphers.Caesar   (caesarDecrypt, caesarEncrypt)
 import           Ciphers.Vigenere (vigenereDecrypt, vigenereEncrypt)
 
